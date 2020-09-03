c72bfc7487ad33eed717176970bed0e481b26637
Sharad Bhandari <chemist.sharad@gmail.com>
2020-09-03 18:08:30 +0545

    crud operation rest api

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from webapp.views import userList
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',userList.as_view())
+    path('webapp/',include('webapp.urls')),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,18 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework import status
 from .models import userInfo
+
 from .serializers import userInfoSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
-class userList(APIView):
-    def get(self,request):
-        user = userInfo.objects.all()
-        serializer = userInfoSerializer(user,many=True)
-        return Response(serializer.data)
+@api_view(['GET'])
+def webappOverview(request):
+    api_urls = {
+        'List':'/task-list',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
 
-    def post(self):
-        pass
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = userInfo.objects.all()
+    serializer = userInfoSerializer(tasks,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks = userInfo.objects.get(id=pk)
+    serializer = userInfoSerializer(tasks,many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = userInfoSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request,pk):
+    task = userInfo.objects.get(id = pk )
+    serializer = userInfoSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request,pk):
+    task = userInfo.objects.get(id = pk )
+    task.delete()
+
+    return Response("items removed successfully!")
